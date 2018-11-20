6e193b9d4aa7e2edaee2539f90a0bcb2ba6e885b
Svetlin <svetlin.stefanov@inoviagroup.se>
2018-11-21 00:41:07 +0100

    Fix metrics.

--- utils/metrics.py
+++ utils/metrics.py
@@ -7,12 +7,10 @@ def mrr(args: argparse.ArgumentParser) -> float:
     preds = pd.read_csv(args.preds, sep=',')
     merged = pd.concat([dataset, preds], axis=1)
 
-    # import pdb; pdb.set_trace()
-
-    df = merged.groupby('question')['actual', 'prob_1'].apply(lambda x: x.sort_values('prob_1', ascending=False)).reset_index()
-    mrr = df.groupby('question')['actual'].apply(lambda x: 1 / (1 + x.tolist().index(1))).values.mean()
+    df = merged.groupby('question')['actual', 'prob_1'].apply(
+        lambda x: x.sort_values('prob_1', ascending=False)).reset_index()
+    mrr = df.groupby('question')['actual'].apply(lambda x: 1 / (1 + x.tolist().index(0))).values.mean()
     return mrr
-
 
 
 def get_args():
