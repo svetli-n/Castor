abefe3de72739cc8e0500f5b6f9fcaf1680784a1
Svetlin <svetlin.stefanov@inoviagroup.se>
2018-11-26 11:05:52 +0100

    Dataset generation performance improvement.

--- utils/create_SQuAD_dataset.py
+++ utils/create_SQuAD_dataset.py
@@ -5,59 +5,54 @@ import pandas as pd
 import numpy as np
 import pickle
 from pandas.io.json import json_normalize
+from tqdm import tqdm
 
 np.random.seed(4241)
 
 
 def to_labeled_pairs(args: argparse.ArgumentParser, dff: pd.DataFrame) -> None:
 
-    all_df = pos_neg_df(args, dff)
+    all_df, num_answers = get_pos_neg_df(args, dff)
 
     all_df.question.to_csv(os.path.join(args.dest, 'a.toks'), index=False, sep='\t')
     all_df.answer.to_csv(os.path.join(args.dest, 'b.toks'), index=False, sep='\t')
     all_df.id.to_csv(os.path.join(args.dest, 'id.txt'), index=False, sep='\t')
     all_df.label.to_csv(os.path.join(args.dest, 'sim.txt'), index=False, sep='\t')
-    print(f'Total:{len(all_df)}:Unique:{len(all_df)/(args.num_neg+1)}')
+
+    print(f'Total:{len(all_df)}:Number of answers:{num_answers}')
 
 
 def to_bert_qnli(args: argparse.ArgumentParser, dff: pd.DataFrame) -> None:
 
-    all_df = pos_neg_df(args, dff)
+    all_df, num_answers = get_pos_neg_df(args, dff)
+
     all_df.label = all_df.label.apply(lambda val: 'entailment' if val == 1 else 'not_entailment')
     all_df.drop(['id'], axis=1, inplace=True)
     all_df.reset_index(drop=False, inplace=True)
     all_df.drop(['index'], axis=1, inplace=True)
     all_df.rename(index=str, columns={'answer': 'sentence'}, inplace=True)
     all_df.to_csv(args.dest, sep='\t', index_label='index')
-    print(f'Total:{len(all_df)}:Unique:{len(all_df)/(args.num_neg+1)}')
+
+    print(f'Total:{len(all_df)}:Number of answers:{num_answers}')
 
 
-def pos_neg_df(args: argparse.ArgumentParser, dff: pd.DataFrame) -> pd.DataFrame:
-    neg_df = pd.DataFrame([], columns=dff.columns)
-    neg_df.id = neg_df.id.astype(int)
-    neg_df.label = neg_df.id.astype(int)
-    cnt = 0
-    num_questions = 0
-    for i, row in dff.iterrows():
-        if num_questions >= args.num_questions:
+def get_pos_neg_df(args: argparse.ArgumentParser, pos_df: pd.DataFrame) -> pd.DataFrame:
+    pos_neg_df = pd.DataFrame([], columns=pos_df.columns)
+    neg_answers_size = min(args.num_neg, len(pos_df)-1)
+    num_answers = neg_answers_size + 1
+
+    for i, row in tqdm(pos_df.iterrows(), total=args.num_questions):
+        if i >= args.num_questions:
             break
-        pos_answer = row['answer']
-        num_neg = args.num_neg
-        while num_neg > 0:
-            rand_i = np.random.randint(0, len(dff) - 1)
-            neg_answer = dff.iloc[rand_i]['answer']
-            if neg_answer != pos_answer:
-                idx = int(dff.iloc[i]['id'])
-                neg_tup = [idx, row['question'], dff.iloc[rand_i]['answer'], 0]
-                neg_df.loc[cnt] = neg_tup
-                num_neg -= 1
-                cnt += 1
-        num_questions += 1
-    all_df = pd.concat([dff[:num_questions], neg_df], ignore_index=True)
-    all_df.reset_index(drop=True, inplace=True)
-    all_df.question = all_df.question.apply(lambda q: str(q))
-    all_df.sort_values(['question'], inplace=True)
-    return all_df
+
+        current_neg_df = pd.concat([pos_df.iloc[0:i], pos_df.iloc[i + 1:len(pos_df)]], ignore_index=True)
+        sampled_current_neg_df = current_neg_df.sample(neg_answers_size)
+        sampled_current_neg_df.question = row.question
+        sampled_current_neg_df.label = 0
+        sampled_current_neg_df = sampled_current_neg_df.append(row, ignore_index=True)
+        pos_neg_df = pd.concat([pos_neg_df, sampled_current_neg_df], ignore_index=True)
+
+    return pos_neg_df, num_answers
 
 
 def to_pickled_dict(args: argparse.ArgumentParser, dff: pd.DataFrame) -> None:
@@ -73,13 +68,11 @@ def to_pickled_dict(args: argparse.ArgumentParser, dff: pd.DataFrame) -> None:
             if rand_neg_answer != pos_answer:
                 out_dict[question].append([rand_neg_answer, 0])
                 num_neg -= 1
-    # TODO CHANGE HyperQA to load data from 3 diff splits
-    result_dict = {'train': out_dict, 'dev': out_dict, 'test': out_dict}
-    # result_dict = {'test': out_dict}
+    result_dict = {'test': out_dict}
 
     if not os.path.exists(args.dest):
         os.mkdir(args.dest)
-    pickle.dump(result_dict, open(os.path.join(args.dest, 'env.pkl'), 'wb'))
+    pickle.dump(result_dict, open(os.path.join(args.dest, 'test.pkl'), 'wb'))
 
 
 def positive_df(args: argparse.ArgumentParser) -> pd.DataFrame:
@@ -118,17 +111,13 @@ if __name__ == '__main__':
 
     args = get_args()
 
-    # if os.path.exists(args.dest):
-    #     shutil.rmtree(args.dest, ignore_errors=True)
-    # os.mkdir(args.dest)
-
     pos_df = positive_df(args)
 
     if args.format == 'castor':
         to_labeled_pairs(args, pos_df)
     elif args.format == 'bert_qnli':
         to_bert_qnli(args, pos_df)
-    elif args.format in ('hyperqa', 'harinder'):
+    elif args.format == 'hyperqa':
         to_pickled_dict(args, pos_df)
     else:
         raise AttributeError('Specify output format')
